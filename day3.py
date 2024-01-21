d559555480bbaa4dfc350ed008eca9b2503fe0c2
Poulav Bhowmick <bpoulav@gmail.com>
2024-01-21 17:01:11 +0530

    day3.1

---
+++ day3.py
@@ -0,0 +1,16 @@
+new_set = set()
+print(set)
+print(type(set))
+
+new = set({'1', '2', '3'})
+print(new)
+
+sets1 = set()
+sets1 = {"a", "b", "c", "d", "e", "f"}
+
+sets2 = set()
+sets2 = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+
+print(sets2.difference(sets1))
+sets1.add("Poulav")
+print(sets1)
