1a3a9bf380d78b3e837547e2f2c5e90f5c7216c7
Poulav Bhowmick <bpoulav@gmail.com>
2024-01-21 11:33:21 +0530

    day2

--- day2.py
+++ day2.py
@@ -4,3 +4,14 @@ print(name.capitalize())
 print(name.endswith("3"))
 # print(name.endswith(3))
 print(name.islower())
+
+#Lists in Python
+# A list is a data structure in python that is changeable, mutable, ordered sequence of 
+# elements
+# Each element of value that is inside a list is called an item.
+# Items in lists are defined by using square brackets
+
+lst = []
+print(type(lst))
+lst.append("a")
+print(lst)
